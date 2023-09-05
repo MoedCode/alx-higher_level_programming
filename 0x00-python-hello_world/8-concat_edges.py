825e71c6b001c70936a2ff23b32e8d39ec17e827
MoedCode <mohamed71291@gmail.com>
2023-09-04 17:32:00 -0700

    (automated command)

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python is cool"
-word = word[:43]
-print(word)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
