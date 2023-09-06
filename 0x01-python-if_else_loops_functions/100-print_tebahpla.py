1f6083098aa20ea3ffa0ad8df1cf7901ba88dfcb
MoedCode <mohamed71291@gmail.com>
2023-09-05 19:56:28 -0700

    ALL ADVANCED TASKS AT ONCE (automated command)

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-    """
-    prints the ASCII alphabet, in reverse order, alternating lowercase 
-    and uppercase (z in lowercase and Y in uppercase) , not followed 
-    by a new line.
-    """
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
