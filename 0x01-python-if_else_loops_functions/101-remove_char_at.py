e85354bec64082e077cf5da4bbad718505b042c2
MoedCode <mohamed71291@gmail.com>
2023-09-05 19:58:02 -0700

    ALL ADVANCED TASKS AT ONCE (automated command)

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    """
-     creates a copy of the string, removing the character at the position n 
-     (not the Python way, the “C array index”).
-    """
+    """creates a copy of the string, removing the character at the position n 
+     (not the Python way, the “C array index”)."""
     if n < 0:
         return (str)
     return (str[:n] + str[n+1:])
