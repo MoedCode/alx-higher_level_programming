6804f4088c9e2e84f4d4ce7f28abe5724ff26c91
MoedCode <mohamed71291@gmail.com>
2023-09-07 17:48:53 -0700

    T5 (automated command)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc = len(sys.argv)
-    if argc < 2:
-        print("{} arguments.".format(argc - 1))
-    elif argc == 2:
-        print("{} argument:".format(argc - 1))
-        print("1: {}".format(sys.argv[1]))
-    else:
-        print("{} arguments:".format(argc - 1))
-        for i in range(1,argc):
-            print("{}: {}".format(i, sys.argv[i]))
+
+argc = len(sys.argv)
+if argc < 2:
+    print("{} arguments.".format(argc-1))
+elif argc == 2:
+    print("{} argument:".format(argc-1))
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(argc-1))
+    for i in range(1, argc):
+        print("{}: {}".format(i, sys.argv[i]))
