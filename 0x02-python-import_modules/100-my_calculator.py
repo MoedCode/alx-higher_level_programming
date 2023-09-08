46847ab6202c4d4b85cf41c3b5e19625ff83efd0
MoedCode <mohamed71291@gmail.com>
2023-09-07 18:43:41 -0700

    AT7  (automated command)

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
     argI1 = int(sys.argv[1])
     argI3 = int(sys.argv[3])
     if sys.argv[2] == "+":
-        print("{} + {} = {}".format(argI1, argI3,add(argI1, argI3)))
+        print("{} + {} = {}".format(argI1, argI3, add(argI1, argI3)))
 
     elif sys.argv[2] == "-":
-        print("{} - {} = {}".format(argI1, argI3,sub(argI1, argI3)))
+        print("{} - {} = {}".format(argI1, argI3, sub(argI1, argI3)))
 
     elif sys.argv[2] == "*":
-        print("{} * {} = {}".format(argI1, argI3,mul(argI1, argI3)))
+        print("{} * {} = {}".format(argI1, argI3, mul(argI1, argI3)))
 
     elif sys.argv[2] == "/":
-        print("{} / {} = {}".format(argI1, argI3,div(argI1, argI3)))
+        print("{} / {} = {}".format(argI1, argI3, div(argI1, argI3)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
