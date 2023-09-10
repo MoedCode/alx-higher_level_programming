504b30896b300f499bd16936d5cb46c8cc5739d2
MoedCode <mohamed71291@gmail.com>
2023-09-10 21:50:23 +0000

    T6 (automated command)

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
-    for row in matrix:
-        for i in range(len(row)):
-            if (i != len(row) - 1):
-                print("{:d}".format(row[i]), end=" ")
+
+    for row in range(len(matrix)):
+        for clm in range(len(matrix[row])):
+            if clm != len(matrix[row])-1:
+                print(matrix[row][clm], end=" ")
             else:
-                print("{:d}".format(row[i]))
+                print("{:d}".format(matrix[row][clm]), end="")
+        print("")
