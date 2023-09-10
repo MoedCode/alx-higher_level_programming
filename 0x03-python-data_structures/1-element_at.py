4560e3556e95cff8d6d7c69352153010d71ffbbe
MoedCode <mohamed71291@gmail.com>
2023-09-10 17:22:35 +0000

    T1 (automated command)

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if (idx > len(my_list)):
-        return (None);
+        return (None)
     for i in range (len(my_list)):
         if(i == idx):
             return (my_list[i])
