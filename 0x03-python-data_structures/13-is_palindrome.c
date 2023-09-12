c2045fb1f51f5b1d4cd986754c3c93d77ba47e57
MoedCode <mohamed71291@gmail.com>
2023-09-12 03:48:37 +0300

    T13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,92 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+/**
+ * palindrome_rec - us recursion to otorate over list
+ * @head: pointer to first node
+ * @tail: pointer to tail norde
+ *
+ * Return: (1)id lists n values is palindrome,
+ *(0) if  it not
+ */
+
+int palindrome_rec(listint_t **head, listint_t *tail)
+{
+	if (!tail)
+		return (1);
+
+	if (palindrome_rec(head, tail->next) == 1 && (*head)->n == tail->n)
+	{
+		(*head) = (*head)->next;
+		return (1);
+	}
+
+	else
+		return (0);
+}
+
+
+/**
+ * is_palindrome - checks if the list is palindrome
+ * @head: pointwr to first node
+ * Return:(1)id lists n values is palindrome,
+ *(0) if  it not
+ */
+
 int is_palindrome(listint_t **head)
 {
-	int len = list_len(*head);
+	if (*head == NULL)
+		return (1);
 
-	listint_t *x = half_list(*head, len);
+	if ((*head)->next == NULL)
+		return (1);
 
-	if (len == 0 || len == 1)
-		return 1;
-
-	reverse_list(&x);
-
-	return compare(head, &(x->next), (len / 2) + 1);
-}
-
-listint_t *half_list(listint_t *head, int len)
-{
-
-	listint_t *curr = head;
-	int index = 0;
-	int is_odd = len % 2 == 0;
-	while (curr)
-	{
-		if (index + 1 == (len / 2))
-		{
-			if (is_odd == 0)
-				return curr->next;
-
-			return curr;
-		}
-
-		curr = curr->next;
-		index++;
-	}
-	return NULL;
-}
-
-int reverse_list(listint_t **head)
-{
-	listint_t *curr = (*head)->next;
-	listint_t *prev = NULL;
-	while (curr != NULL)
-	{
-		listint_t *next = curr->next;
-
-		curr->next = prev;
-
-		/*update previous*/
-		prev = curr;
-
-		/*increment */
-		curr = next;
-	}
-	/*links the first node to the last reversed node */
-	(*head)->next = prev;
-
-	return 0;
-}
-
-int compare(listint_t **head1, listint_t **head2, int n)
-{
-	listint_t *curr1 = *head1;
-	listint_t *curr2 = *head2;
-	int index = 0;
-
-	while (curr1 != NULL && curr2 != NULL)
-	{
-
-		if (curr1->n != curr2->n)
-			return 0;
-
-		if (index == n)
-			break;
-
-		curr1 = curr1->next;
-		curr2 = curr2->next;
-		index++;
-	}
-	return 1;
-}
-
-int list_len(listint_t *head)
-{
-	int len = 0;
-	listint_t *curr = head;
-	while (curr)
-	{
-		len++;
-		curr = curr->next;
-	}
-	return len;
+	return (palindrome_rec(head, *head));
 }
