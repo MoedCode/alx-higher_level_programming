6624ca7e854150290baf97e810fae11fdeb593ae
MoedCode <mohamed71291@gmail.com>
2023-09-25 21:55:11 +0300

    tray exept block (automated command)

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,12 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            # Try to print the element at index i if it exists
+            print(my_list[i], end="")
             printed_count += 1
     except IndexError:
-        pass  # Handle the case where x is greater than the length of my_list
+        # If an IndexError occurs (i.e., index out of range), just continue
+        pass
 
-    print()
+    print()  # Print a new line to end the output
     return printed_count
