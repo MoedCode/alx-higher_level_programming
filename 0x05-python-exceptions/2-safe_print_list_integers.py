50ba135b929b1a7f0fc4eab26aa38097a2652159
MoedCode <mohamed71291@gmail.com>
2023-09-26 01:48:12 +0300

    Try Except T1  (automated command)

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        if isinstance(value, int):
-            return True
-        else:
-            return False
-    except Exception:
-        return False
-
-
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            if safe_print_integer(my_list[i]):
-                print(my_list[i], end="")
-                count += 1
-        except IndexError:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
             continue
 
-    print()
+    print("")
     return count
