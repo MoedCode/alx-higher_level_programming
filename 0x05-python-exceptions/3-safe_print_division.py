eb15c49c88b2118de3ddd215e8694981b134cf0a
MoedCode <mohamed71291@gmail.com>
2023-09-26 03:35:48 +0300

    Try Except T1  (automated command)

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,6 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a/b
-        print("{:d} / {:d} = {:.1f}".format(a, b, result))
     except ZeroDivisionError:
         pass
     finally:
