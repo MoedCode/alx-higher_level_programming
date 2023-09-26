e4fe21ce1d6d5dcfbbdab3e63a45da7c817d79ba
MoedCode <mohamed71291@gmail.com>
2023-09-26 05:12:27 +0300

    C isnt that fun weget  tierd 😮‍💨  (automated command)

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,9 +2,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        int_value = int(value)
-        print("{:d}".format(int_value))
+        print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
