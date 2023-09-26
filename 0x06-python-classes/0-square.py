e098edc78caf83bda4708444e07c77fae0e8d2ee
MoedCode <mohamed71291@gmail.com>
2023-09-27 02:39:08 +0300

    (automated command)

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
+Square = __import__('0-square').Square
 
-class Square:
-    """
-    This is the Square class.
-    """
-
-    def __init__(self):
-        """
-        This is the constructor method for Square.
-        """
-        pass
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
