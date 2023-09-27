c5dd5a0ae2c4c72d5336603166fc7a09e72a3bc5
MoedCode <mohamed71291@gmail.com>
2023-09-27 05:02:10 +0300

    T1 ; T2 ; T3 (automated command)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+"""Square class"""
+
 
 class Square:
+    """Square class"""
+
     def __init__(self, size=0):
         self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
 
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+    def size(self, size):
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        return self.__size ** 2
+        self.__size = size
