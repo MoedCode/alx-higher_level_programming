720a02030bab89c9081b1a22c5c7fb38067d1b2f
MoedCode <mohamed71291@gmail.com>
2023-09-27 03:32:57 +0300

    T1 ; T2 ; T3 (automated command)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """class  constructor function"""
+
+    def __init__(self, size=0):
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        self.__size = size
+
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
