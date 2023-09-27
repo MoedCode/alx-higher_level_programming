da92a00b6c776c06bc32f7356863106d5efd5cd5
MoedCode <mohamed71291@gmail.com>
2023-09-27 05:09:20 +0300

    T1 ; T2 ; T3 (automated command)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 
 class Square:
+    """Square class with getter, setter, and area method."""
+
     def __init__(self, size=0):
+        """Initialize a Square instance.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        """
         self.__size = size
 
     @property
     def size(self):
+        """Get the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size to set.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -18,4 +34,19 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Calculate and return the area of the square."""
         return self.__size ** 2
+
+
+if __name__ == "__main__":
+    my_square = Square(89)
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    my_square.size = 3
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    try:
+        my_square.size = "5 feet"
+        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    except Exception as e:
+        print(e)
