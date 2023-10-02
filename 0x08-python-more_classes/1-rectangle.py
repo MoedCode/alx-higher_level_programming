74c6a408b76b34358a85e82333adaa5a608c8f99
MoedCode <mohamed71291@gmail.com>
2023-10-03 01:25:43 +0300

    👉 👈 (automated command)

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+ Rectangle Module:
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class:
+    it has two attributes that get , set width and height
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Width must be an integer')
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
