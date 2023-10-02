39fd2a4bede16907e8d749ea8fc9c1f2d5b2024a
MoedCode <mohamed71291@gmail.com>
2023-10-02 17:44:33 +0300

    👉 👈 (automated command)

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+
+"""
+Module:say_my_name
+prints a full name
+"""
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name prints 2 inputs from type strings with "My name is".
+    Raises:
+    TypeError: If `first_name` or `last_name` are not strings.
+    It accepts one or two arguments.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
