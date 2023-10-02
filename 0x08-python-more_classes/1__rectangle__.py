b0f9c6d982462f14d7532aa2bc2dfd39479d63a3
MoedCode <mohamed71291@gmail.com>
2023-10-03 01:37:44 +0300

    👉 👈 (automated command)

--- 0x08-python-more_classes/1__rectangle__.py
+++ 0x08-python-more_classes/1__rectangle__.py
@@ -11,40 +11,35 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-
+        if not isinstance(width, int) or not isinstance(height, int):
+            wrong = 'width' if not isinstance(width, int) else 'height'
+            raise TypeError(wrong + 'must be an integer')
+        if height < 0 or width < 0:
+            wrong = 'width' if width < 0 else 'height'
+            raise ValueError(wrong + 'must be >= 0')
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
     def height(self):
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError('height must be >= 0')
         self.__height = value
-
-
-# Example usage:
-if __name__ == "__main__":
-    my_rectangle = Rectangle(2, 4)
-    print(my_rectangle.__dict__)
-
-    my_rectangle.width = 10
-    my_rectangle.height = 3
-    print(my_rectangle.__dict__)
