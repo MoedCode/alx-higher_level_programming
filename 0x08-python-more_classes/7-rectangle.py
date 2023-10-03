5d2842e032c581752dd0420d2644eebfd77feff4
MoedCode <mohamed71291@gmail.com>
2023-10-03 05:11:33 +0300

    👉 👈 (automated command)

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -122,9 +122,9 @@ class Rectangle:
         rectangle = []
         for i in range(self.__height):
             for j in range(self.__width):
-                rectangle.append(str(self.print_symbol))
+                rectangle += [str(self.print_symbol)]
             if i is not self.__height - 1:
-                rectangle.append('\n')
+                rectangle += ['\n']
         return ''.join(rectangle)
 
     def __repr__(self):
