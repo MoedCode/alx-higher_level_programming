842eca890247a857b603455e720e66b7e8b93177
MoedCode <mohamed71291@gmail.com>
2023-10-03 04:54:33 +0300

    👉 👈 (automated command)

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,45 +1,42 @@
 #!/usr/bin/python3
 """
- Rectangle Module:
+The Rectangle Module:
 """
 
 
 class Rectangle:
     """
     Rectangle class:
-    it has two attributes that get , set width and height
-    Rectangle class:
     it has two attributes width and height
     """
-
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         if not isinstance(width, int) or not isinstance(height, int):
-            TEM = 'width' if not isinstance(width, int) else 'height'
-            raise TypeError(TEM + 'must be an integer')
+            wrong = 'width' if not isinstance(width, int) else 'height'
+            raise TypeError(wrong + 'must be an integer')
         if height < 0 or width < 0:
-            VEM = 'width' if width < 0 else 'height'
-            raise ValueError(VEM + 'must be >= 0')
+                wrong = 'width' if width < 0 else 'height'
+                raise ValueError(wrong + 'must be >= 0')
         self.__width = width
         self.__height = height
 
     def __str__(self):
-        """printing and using str() functions"""
+        """Functionality for printing and using str() functions"""
 
         if self.__width == 0 or self.__height == 0:
             return ""
         rectangle = []
         for i in range(self.__height):
             for j in range(self.__width):
-                rectangle += ['#']
+                rectangle.append('#')
             if i is not self.__height - 1:
-                rectangle += ['\n']
-        return ''.join(rectangle)
+                rectangle.append('\n')
+        return''.join(rectangle)
 
     @property
     def width(self):
-        return (self.__width)
+        return(self.__width)
 
     @width.setter
     def width(self, value):
@@ -51,7 +48,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return (self.__height)
+        return(self.__height)
 
     @height.setter
     def height(self, value):
@@ -62,13 +59,13 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return (self.__width * self.__height)
+        return(self.__width * self.__height)
 
     def perimeter(self):
         if self.__width > 0 and self.__height > 0:
-            return (self.__width * 2 + self.__height * 2)
+            return(self.__width * 2 + self.__height * 2)
         else:
-            return (0)
+            return(0)
 
     def __repr__(self):
         return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
