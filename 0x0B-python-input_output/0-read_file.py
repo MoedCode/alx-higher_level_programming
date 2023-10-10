4587a690e0b955433639b0ec327739bcfda86f08
MoedCode <mohamed71291@gmail.com>
2023-10-11 00:56:23 +0300

    👉 👈 0x0x0B-python-input_output T0 (automated command)

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     "function reads from a file"
-    with open(filename, "r", encoding="UTF8") as file:
-        for line in file:
+    with open(filename, "r", encoding="UTF8") as FILE:
+        for line in FILE:
             print(line, end="")
