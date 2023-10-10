9e83043b84579ff4d89529c4dc51ffa21f1ce035
MoedCode <mohamed71291@gmail.com>
2023-10-11 02:03:49 +0300

    👉 👈 0x0x0B-python-input_output T1 (automated command)

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module counts the number of lines of a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    write_file: write given buffer to file
+    if file does not exist it creates one
+    with a given file name
+    text: buffer to be written to file
+    filename: target file where buffer is to be written
+    returns:the number of lines of a text file
+    """
+    with open(filename, "a") as file:
+        return file.write(text)
