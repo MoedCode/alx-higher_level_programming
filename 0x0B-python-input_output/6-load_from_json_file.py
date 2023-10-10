3c33148de14a5402d12a8a4c3a2d639e9cf406b3
MoedCode <mohamed71291@gmail.com>
2023-10-11 02:45:57 +0300

    👉 👈 0x0x0B-python-input_output T6 (automated command)

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,11 +6,11 @@ module:  writes an Object to a text file
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """save a given Object to a text file  with a given name,
-    using a JSON representation
+def load_from_json_file(filename):
+    """read  given file contain  a json object,
+    pares it to object return it
     my_pbj: a given object
     filename : str, the name of the output file
     """
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
+    with open(filename, 'r', encoding='utf-8') as FILE:
+        return json.load(FILE)
