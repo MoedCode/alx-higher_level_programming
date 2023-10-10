8e9af3364039fbf99731cb362dd8d0fbbb447fd8
MoedCode <mohamed71291@gmail.com>
2023-10-11 01:08:48 +0300

    👉 👈 0x0x0B-python-input_output T1 (automated command)

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,7 +5,7 @@
 def number_of_lines(filename=""):
     "returns the number of lines of a text file"
     with open(filename, "r", encoding="UTF8") as FILE:
-        LINE = 0;
+        LINE = 0
         for line in FILE:
             LINE++
     return LINE
