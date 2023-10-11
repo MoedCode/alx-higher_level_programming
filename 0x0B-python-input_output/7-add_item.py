39803c37ede996d4b0ca3bbfb700d856cad58ee8
MoedCode <mohamed71291@gmail.com>
2023-10-11 04:32:35 +0300

    👉 👈 0x0x0B-python-input_output T7 (automated command)

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,25 +4,25 @@ script that adds all arguments to list,
 and then save them to a file:
 """
 import sys
+import os
 import os.path
-import json
+""" os.path module"""
+"""  os module"""
 
 Path0 = "add_item.json"
-""" function that load_from_json_file """
+
+"""  load_from_json_file """
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-""" function that save_to_json_file """
+"""  save_to_json_file """
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-if os.path.exists(Path0):
-    with open(Path0, mode="r", encoding="utf-8") as FILE:
-        save_to_json_file([], Path0)
 
-""" copy tje file to _argv"""
-_argv = load_from_json_file(Path0)
+if not os.path.exists("path0") or os.stat("path0").st_size == 0:
+    save_to_json_file([], "path0")
 
-"""  Append arguments to the list"""
-for _ in range(1, len(sys.argv)):
-    _argv += [sys.argv[_]]
+_argv = load_from_json_file("path0")
 
-"Save the updated list to the JSON file"
-save_to_json_file(_argv, Path0)
+for i in range(1, len(sys.argv)):
+    _argv += [sys.argv[i]]
+
+save_to_json_file(_argv, "path0")
