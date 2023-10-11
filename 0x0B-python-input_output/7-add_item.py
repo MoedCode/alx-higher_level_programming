5b43c5af413cd7e7d095389051ec18d57cdc2aed
MoedCode <mohamed71291@gmail.com>
2023-10-11 04:33:45 +0300

    👉 👈 0x0x0B-python-input_output T7 (automated command)

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 """
-script that adds all arguments to list,
-and then save them to a file:
+Script that adds all arguments to a Python list, and then saves them to a file
 """
-import sys
-import os
-import os.path
-""" os.path module"""
-"""  os module"""
 
-Path0 = "add_item.json"
-
-"""  load_from_json_file """
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-"""  save_to_json_file """
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+filename = "add_item.json"
 
-if not os.path.exists("path0") or os.stat("path0").st_size == 0:
-    save_to_json_file([], "path0")
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
 
-_argv = load_from_json_file("path0")
+for arg in argv[1:]:
+    json_list.append(arg)
 
-for i in range(1, len(sys.argv)):
-    _argv += [sys.argv[i]]
-
-save_to_json_file(_argv, "path0")
+save_to_json_file(json_list, filename)
