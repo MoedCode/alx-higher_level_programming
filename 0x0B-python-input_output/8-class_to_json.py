a37cd982aaa447efe923d2cd0245e7e93dc748da
MoedCode <mohamed71291@gmail.com>
2023-10-12 05:43:50 +0300

    👉 👈 0x0x0B-python-input_output T14 (automated command)

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,4 +4,27 @@
 
 def class_to_json(obj):
     """Return the dictionary representation of a simple data structure."""
-    return obj.__dict__
+    return obj.__str__()
+
+
+if __name__ == "__main__":
+
+    class MyClass:
+        """ My class
+        """
+
+        def __init__(self, name):
+            self.name = name
+            self.number = 0
+
+        def __str__(self):
+            return "[MyClass] {} - {:d}".format(self.name, self.number)
+
+    m = MyClass("John")
+    m.number = 89
+    print(type(m))
+    print(m)
+
+    mj = class_to_json(m)
+    print(type(mj))
+    print(mj)
