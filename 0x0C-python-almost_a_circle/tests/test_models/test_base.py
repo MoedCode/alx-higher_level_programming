dce5edb00f654f01a16bb18d22a1807484b712d1
MoedCode <mohamed71291@gmail.com>
2023-10-17 04:35:38 +0300

    10/16/2023 12:51  (automated command)

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Module for test Base class """
+"""Module for testing the Base class and its related functionality."""
 import unittest
+import os
 from models.base import Base
 from models.square import Square
 from models.rectangle import Rectangle
@@ -8,60 +9,59 @@ from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
 
-
 class TestBaseMethods(unittest.TestCase):
-    """ Suite to test Base class """
+    """Test suite for the Base class and its methods."""
 
     def setUp(self):
-        """ Method invoked for each test """
+        """Set up method executed before each test."""
         Base._Base__nb_objects = 0
 
-    def test_id(self):
-        """ Test assigned id """
-        new = Base(1)
-        self.assertEqual(new.id, 1)
+    def test_id_assignment(self):
+        """Test the assignment of an ID to a Base instance."""
+        new_instance = Base(1)
+        self.assertEqual(new_instance.id, 1)
 
-    def test_id_default(self):
-        """ Test default id """
-        new = Base()
-        self.assertEqual(new.id, 1)
+    def test_default_id(self):
+        """Test the default ID assigned to a Base instance."""
+        new_instance = Base()
+        self.assertEqual(new_instance.id, 1)
 
-    def test_id_nb_objects(self):
-        """ Test nb object attribute """
-        new = Base()
-        new2 = Base()
-        new3 = Base()
-        self.assertEqual(new.id, 1)
-        self.assertEqual(new2.id, 2)
-        self.assertEqual(new3.id, 3)
+    def test_incrementing_id(self):
+        """Test that the ID increments for each new Base instance."""
+        new_instance1 = Base()
+        new_instance2 = Base()
+        new_instance3 = Base()
+        self.assertEqual(new_instance1.id, 1)
+        self.assertEqual(new_instance2.id, 2)
+        self.assertEqual(new_instance3.id, 3)
 
-    def test_id_mix(self):
-        """ Test nb object attributes and assigned id """
-        new = Base()
-        new2 = Base(1024)
-        new3 = Base()
-        self.assertEqual(new.id, 1)
-        self.assertEqual(new2.id, 1024)
-        self.assertEqual(new3.id, 2)
+    def test_mix_of_assigned_and_incremented_id(self):
+        """Test a mix of assigned and incremented IDs."""
+        new_instance1 = Base()
+        new_instance2 = Base(1024)
+        new_instance3 = Base()
+        self.assertEqual(new_instance1.id, 1)
+        self.assertEqual(new_instance2.id, 1024)
+        self.assertEqual(new_instance3.id, 2)
 
     def test_string_id(self):
-        """ Test string id """
-        new = Base('1')
-        self.assertEqual(new.id, '1')
+        """Test creating a Base instance with a string as ID."""
+        new_instance = Base('1')
+        self.assertEqual(new_instance.id, '1')
 
-    def test_more_args_id(self):
-        """ Test passing more args to init method """
+    def test_more_args_in_init(self):
+        """Test passing more arguments to the init method than allowed."""
         with self.assertRaises(TypeError):
-            new = Base(1, 1)
+            new_instance = Base(1, 1)
 
-    def test_access_private_attrs(self):
-        """ Test accessing to private attributes """
-        new = Base()
+    def test_accessing_private_attributes(self):
+        """Test attempting to access private attributes of a Base instance."""
+        new_instance = Base()
         with self.assertRaises(AttributeError):
-            new.__nb_objects
+            new_instance.__nb_objects
 
-    def test_save_to_file_1(self):
-        """ Test JSON file """
+    def test_save_to_file_for_Square(self):
+        """Test saving data to a JSON file for Square instances."""
         Square.save_to_file(None)
         res = "[]\n"
         with open("Square.json", "r") as file:
@@ -78,8 +78,8 @@ class TestBaseMethods(unittest.TestCase):
         with open("Square.json", "r") as file:
             self.assertEqual(file.read(), "[]")
 
-    def test_save_to_file_2(self):
-        """ Test JSON file """
+    def test_save_to_file_for_Rectangle(self):
+        """Test saving data to a JSON file for Rectangle instances."""
         Rectangle.save_to_file(None)
         res = "[]\n"
         with open("Rectangle.json", "r") as file:
