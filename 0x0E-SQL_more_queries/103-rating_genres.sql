b54e5fe618204c93ad99481ea63658d07022012b
MoedCode <mohamed71291@gmail.com>
2023-11-16 04:24:16 +0200

    SQL 0XD T20 (automated command)

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,12 +1,11 @@
--- Lists all genres in the 'hbtn_0d_tvshows_rate' database by their rating.
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Records are ordered by descending rating.
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
 
--- Each record should display: tv_genres.name - rating sum.
--- Results must be sorted in descending order by their rating.
-
-SELECT g.`name`, SUM(r.`rating`) AS `rating`
-FROM `tv_genres` AS g
-JOIN `tv_show_genres` AS sg ON g.`id` = sg.`genre_id`
-JOIN `tv_shows` AS s ON sg.`show_id` = s.`id`
-LEFT JOIN `tvshows_rate` AS r ON s.`id` = r.`show_id`
-GROUP BY g.`id`, g.`name`
-ORDER BY `rating` DESC;
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
