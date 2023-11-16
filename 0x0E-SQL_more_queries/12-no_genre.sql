1e2097bb22471e4e5ac44a73d23c7536b3d50aa9
MoedCode <mohamed71291@gmail.com>
2023-11-16 04:08:31 +0200

    SQL 0XD T11 (automated command)

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,13 @@
+-- List all shows contained in the 'hbtn_0d_tvshows' database without a genre linked.
+
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id.
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+-- Only one SELECT statement is allowed.
+
+-- The database name will be provided as an argument to the mysql command.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
