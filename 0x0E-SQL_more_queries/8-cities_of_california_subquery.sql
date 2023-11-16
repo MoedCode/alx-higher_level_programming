c3a3c1e41b227e1cb6220e55cffe30586300f267
MoedCode <mohamed71291@gmail.com>
2023-11-16 03:51:45 +0200

    SQL 0XD T8 (automated command)

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,17 @@
--- List all the cities of California in the database 'hbtn_0d_usa'.
+-- Retrieve a list of all cities located in California from the 'hbtn_0d_usa' database.
 
--- The states table contains only one record where name = California.
--- Results must be sorted in ascending order by cities.id.
--- You are not allowed to use the JOIN keyword.
+-- The 'states' table contains only one record where the name is 'California'
+-- (the id can vary, as illustrated in the example).
+
+-- The results must be sorted in ascending order based on the 'id' column of the 'cities' table.
+-- The use of the JOIN keyword is not allowed.
 
 -- The database name will be provided as an argument to the mysql command.
 
-SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC;
+SELECT `id`, `name`
+FROM `cities`
+WHERE `state_id` IN
+    (SELECT `id`
+	FROM `states`
+	WHERE `name` = 'California')
+ORDER BY `id`;
