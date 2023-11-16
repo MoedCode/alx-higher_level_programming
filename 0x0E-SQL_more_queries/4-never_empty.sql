f65703e3cba24b84848327533e00b25d9374a5b9
MoedCode <mohamed71291@gmail.com>
2023-11-16 03:30:32 +0200

    SQL 0XD T4 (automated command)

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,13 @@
--- 3-force_name.sql
--- Create the table force_name if it doesn't exist.
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
+-- Create the table 'id_not_null' on your MySQL server.
+
+-- Description of 'id_not_null':
+--     - 'id' is an integer (INT) with the default value of 1.
+--     - 'name' is a variable character (VARCHAR) with a length of 256 characters.
+
+-- The database name will be provided as an argument to the mysql command.
+-- If the table 'id_not_null' already exists, the script should not fail.
+
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT 1,
     name VARCHAR(256) NOT NULL
 );
