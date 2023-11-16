2159b30352346574fd22462d9abc10a95a37d0d3
MoedCode <mohamed71291@gmail.com>
2023-11-16 03:35:55 +0200

    SQL 0XD T6 (automated command)

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,20 +1,17 @@
--- Create the database 'hbtn_0d_usa' and the table 'states' in the database on your MySQL server.
+-- Create the database 'hbtn_0d_usa' and the table 'states' within it on your MySQL server.
 
 -- Description of 'states':
---     - 'id' is an integer (INT) unique, auto-generated, cannot be null, and is a primary key.
+--     - 'id' is an integer (INT) that is unique, auto-generated, cannot be null, and serves as the primary key.
 --     - 'name' is a variable character (VARCHAR) with a length of 256 characters and cannot be null.
 
 -- If the database 'hbtn_0d_usa' already exists, the script should not fail.
--- If the table 'states' already exists, the script should not fail.
+-- If the table 'states' already exists within the database, the script should not fail.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
-
--- Inserting sample data
-INSERT INTO states (name) VALUES ('California'), ('Arizona'), ('Texas');
