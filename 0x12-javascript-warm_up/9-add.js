2fd9aa8dfc33afbd61ca675eb6e1aba867b995c4
MoedCode <mohamed71291@gmail.com>
2023-12-11 21:12:31 +0200

    (automated command)

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const args = process.argv;
+
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN');
+    return;
+  }
+  console.log(parseInt(a) + parseInt(b));
+}
+add(args[2], args[3]);
