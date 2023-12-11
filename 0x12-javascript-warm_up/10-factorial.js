fa6c237e4e095719841ed9b52c6d752c65a0924d
MoedCode <mohamed71291@gmail.com>
2023-12-11 21:53:10 +0200

    (automated command)

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const N = parseInt(process.argv[2]);
+
+if (isNaN(N) || N === 0) {
+  console.log(1);
+} else {
+  function getFact (n) {
+    if (n >= 1) {
+      return n * getFact(n - 1);
+    } else {
+      return 1;
+    }
+  }
+
+  console.log(getFact(N));
+}
