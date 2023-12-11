352b6cc719286f83811df29bac61502cd75f9038
MoedCode <mohamed71291@gmail.com>
2023-12-11 18:29:08 +0200

    2. Arguments (automated command)

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,12 @@
 const { argv } = require('node:process');
 
 const len = argv.length;
-if (len < 3) { console.log('No argument'); } else if (len >= 4) { console.log('Argument found'); }
+console.log(`len => ${len}`);
+if (len < 3)
+{
+    console.log('No argument');
+}
+else if (len >= 3)
+{
+     console.log('Argument found');
+ }
