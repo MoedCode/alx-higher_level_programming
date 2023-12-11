bee53aeb4b1959a5eff06dbe31fb43253ba66024
MoedCode <mohamed71291@gmail.com>
2023-12-11 21:02:42 +0200

    (automated command)

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 
 if (isNaN(args[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   let str = '';
   for (let i = 0; i < parseInt(args[2]); i++) {
