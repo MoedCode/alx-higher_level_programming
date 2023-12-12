7ff9efe4c54e0ca10d1c8b1d81774d0306b4c5e6
MoedCode <mohamed71291@gmail.com>
2023-12-12 21:29:16 +0200

    JS classes Tasks 0, 1, 2, 3 (automated command)

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isFinite(h)) {
+      this.width = w;
+      this.height = h;
+      this.print = function () {
+        let str = '';
+        for (let i = 0; i < this.width; i++) {
+          str += 'X';
+        }
+        for (let i = 0; i < this.height; i++) {
+          console.log(str);
+        }
+      };
+    }
+  }
+}
+
+module.exports = Rectangle;
