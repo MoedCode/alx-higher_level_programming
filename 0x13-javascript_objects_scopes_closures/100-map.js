69c7ac0cee8d154d6ed1cd8c7caccd999f90d98d
MoedCode <mohamed71291@gmail.com>
2023-12-13 02:49:13 +0200

    JS classes Tasks  11 (automated command)

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const cursedList = require('./100-data').list;
+
+const damenList = [];
+for (let i = 0; i < cursedList.length; i++) {
+  damenList[i] = cursedList[i] * i;
+}
+console.log(cursedList);
+console.log(damenList);
