c228737b20e8dfd1c184d019e9cd4137d6b1c345
MoedCode <mohamed71291@gmail.com>
2023-12-15 21:43:05 +0200

    0x0F. PY ORM T 2 (automated command)

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+"""Write a script that lists
+all states from the database
+ hbtn_0e_0_usa
+ """
 
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
     dp = MySQLdb.connect(
-        host="localhost", port="3306", user=argv[1], passwd=argv[2], dp=argv[3])
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], dp=argv[3])
     cur = dp.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-            ORDER BY states.id ASC;".format(argv[4]))
+    qrr = "SELECT * FROM states WHERE name LIKE BINARY '{}'\
+            ORDER BY states.id ASC;".format(argv[4])
+    cur.execute(qrr)
     rows = cur.fetchall()
     for _ in rows:
         print(_)
