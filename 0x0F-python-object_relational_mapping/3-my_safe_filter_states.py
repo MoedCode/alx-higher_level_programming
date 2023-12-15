4f62c7787b88ed93eec0524f5e5f44b3891ee109
MoedCode <mohamed71291@gmail.com>
2023-12-15 23:30:40 +0200

    0x0F. PY ORM T 3 (automated command)

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,9 +4,10 @@ script that takes in arguments and displays all values in
 the states table of hbtn_0e_0_usa where name matches the argument.
 But this time, write one that is safe from MySQL injections!
 """
-import MySQLdb
-from sys import argv
 if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
     dp = MySQLdb.Connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], dp=argv[3])
     cursor = dp.cursor()
