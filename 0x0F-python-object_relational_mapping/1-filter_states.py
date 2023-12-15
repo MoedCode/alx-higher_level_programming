bb8a6b988d10bdc1598ad74a9b2e74e911ddd280
MoedCode <mohamed71291@gmail.com>
2023-12-15 21:13:02 +0200

    0x0F. PY ORM T 2 (automated command)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
+
+    cur.execute(
+        """SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    dp = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], dp=argv[3], port="3306")
+    cur = dp.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY  '{}' ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for _ in rows:
+        print(_)
