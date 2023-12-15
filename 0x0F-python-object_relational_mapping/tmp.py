7b129d6946bf8e4babf4fa7b466fb916d3793a33
MoedCode <mohamed71291@gmail.com>
2023-12-15 23:46:45 +0200

    0x0F. PY ORM T 3 (automated command)

--- 0x0F-python-object_relational_mapping/tmp.py
+++ 0x0F-python-object_relational_mapping/tmp.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     namae = argv[4]
     db = MySQLdb.Connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], dp=argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE %s \
                     ORDER BY id ASC", (argv[4], ))
