c680e04631a31a440ba71d08d3a33b95220458be
MoedCode <mohamed71291@gmail.com>
2023-12-15 17:05:38 +0200

    0x0F. PY ORM T 1 (automated command)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    # Establish a connection to the MySQL server
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    # Create a cursor object to execute SQL queries
+    cur = db.cursor()
+
+    # Execute the SQL query to select states starting with 'N'
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+
+    # Fetch all rows from the result set
+    rows = cur.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
