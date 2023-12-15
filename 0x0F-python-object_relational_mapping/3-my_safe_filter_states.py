0a94d9c58bd49ce388600eeeda86a9276ac05642
MoedCode <mohamed71291@gmail.com>
2023-12-15 23:37:10 +0200

    0x0F. PY ORM T 3 (automated command)

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], dp=argv[3])
     cursor = db.cursor()
     cursor.execute(
-        "    SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (argv[4], ))
+        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (argv[4], ))
 
     rows = cursor.fetchall()
     for row in rows:
