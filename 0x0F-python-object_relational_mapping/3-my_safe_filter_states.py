2ceef94ee44c7101005be67fcb40ebba988ea038
MoedCode <mohamed71291@gmail.com>
2023-12-15 23:39:55 +0200

    0x0F. PY ORM T 3 (automated command)

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     db = MySQLdb.Connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], dp=argv[3])
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (argv[4], ))
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s\
+                    ORDER BY id ASC", (argv[4], ))
 
     rows = cursor.fetchall()
     for row in rows:
