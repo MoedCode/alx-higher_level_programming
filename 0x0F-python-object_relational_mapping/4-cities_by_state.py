5e8bb482e20f8d38dc5fb54a1912ad2765b881a1
MoedCode <mohamed71291@gmail.com>
2023-12-16 01:31:32 +0200

    0x0F. PY ORM T 4 (automated command)

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+ lists all cities from the database hbtn_0e_4_usa
+ Results must be sorted in ascending order by cities.id
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
