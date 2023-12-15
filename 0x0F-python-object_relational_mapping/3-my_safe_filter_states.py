48cad25d7f6c5e99ddb4df68ceb9af7b3a6271a2
MoedCode <mohamed71291@gmail.com>
2023-12-15 23:09:05 +0200

    0x0F. PY ORM T 3 (automated command)

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+script that takes in arguments and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
+"""
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    dp = MySQLdb.Connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], dp=argv[3])
+    cursor = dp.cursor()
+    cursor.execute(
+        "    SELECT * FROM states WHERE LIKE %s ORDER BY id ASC", (argv[4]))
+    rows = cursor.fetchall()
+    for _ in rows:
+        print(_)
