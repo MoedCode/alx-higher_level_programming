810d0951cdd52eee1ec7a9c77ee0bb036b6301b4
MoedCode <mohamed71291@gmail.com>
2023-12-16 02:06:53 +0200

    0x0F. PY ORM T 4 (automated command)

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" lists all cities from the database hbtn_0e_4_usa  """
+"""Write a script that lists
+all states from the database
+ hbtn_0e_0_usa"""
 
 if __name__ == "__main__":
     import MySQLdb
