ad28ccaa85c2b93bd4bc4790e835d30f8d5b55a0
MoedCode <mohamed71291@gmail.com>
2023-12-16 02:06:02 +0200

    0x0F. PY ORM T 4 (automated command)

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
- lists all cities from the database hbtn_0e_4_usa
- Results must be sorted in ascending order by cities.id
-"""
+""" lists all cities from the database hbtn_0e_4_usa  """
 
 if __name__ == "__main__":
     import MySQLdb
@@ -11,7 +8,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    # cursor.execute("SELECT * FROM cities ORDER BY id ASC")
     quarry_exe = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id=cities.state_id;"
     cursor.execute(quarry_exe)
 
