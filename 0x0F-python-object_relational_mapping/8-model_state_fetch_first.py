656250d61a9fd9d93476454e1f5f8917a69c1e18
MoedCode <mohamed71291@gmail.com>
2023-12-17 00:23:32 +0200

    0x0F. PY ORM T 8 (automated command)

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine("musql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_list = session.query(State).order_by(State.id).first()
+    if not state_list:
+        print("Nothing")
+    else:
+        print(f"{list_State.id}: {list_State.name}")
+    session.close()
