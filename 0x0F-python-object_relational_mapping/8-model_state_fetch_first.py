22a87df4fd00df7c81f068f6b3f06ce52deda4f2
MoedCode <mohamed71291@gmail.com>
2023-12-17 01:56:34 +0200

    0x0F. PY ORM T 8 (automated command)

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
+"""
+     prints the first State object from the database hbtn_0e_6_usa
+"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # engine = create_engine("musql+mysqldb://{}:{}@localhost:3306/{}"
-    #    .format(argv[1], argv[2], argv[3]),
-    #    pool_pre_ping=True)
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+
     state_list = session.query(State).order_by(State.id).first()
-    if not state_list:
+    if state_list is None:
         print("Nothing")
     else:
         print(f"{state_list.id}: {state_list.name}")
