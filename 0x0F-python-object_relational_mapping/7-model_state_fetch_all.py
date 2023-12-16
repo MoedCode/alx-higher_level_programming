4fe3bea44f803f86327957a64da931ae0dcbf713
MoedCode <mohamed71291@gmail.com>
2023-12-16 22:09:12 +0200

    0x0F. PY ORM T 7 (automated command)

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    # Creating the engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Creating the tables
+    Base.metadata.create_all(engine)
+
+    # Using the session in a with statement
+    with Session(engine) as session:
+        # Querying and printing data
+        states = session.query(State).order_by(State.id).all()
+        for each_state in states:
+            print("{}: {}".format(each_state.id, each_state.name))
--- 0x0F-python-object_relational_mapping/tmp.py
+++ 0x0F-python-object_relational_mapping/tmp.py
@@ -1,23 +1,23 @@
 +--------+
-| name   |
+| name |
 +--------+
 | Dallas |
 | Houston|
 | Austin |
 +--------+
 +----+-----------+
-| id |   name    |
+| id | name |
 +----+-----------+
-| 1  |  California |
-| 2  |   Arizona   |
-| 3  |    Texas    |
+| 1 | California |
+| 2 | Arizona |
+| 3 | Texas |
 +----+-----------+
 +----+-----------+----------+
-| id | state_id  |  name    |
+| id | state_id | name |
 +----+-----------+----------+
-| 1  |    1      | San Francisco |
-| 2  |    1      |   San Jose   |
-| 3  |    2      |    Page      |
-| 4  |    3      |   Dallas     |
-| 5  |    3      |   Houston    |
+| 1 | 1 | San Francisco |
+| 2 | 1 | San Jose |
+| 3 | 2 | Page |
+| 4 | 3 | Dallas |
+| 5 | 3 | Houston |
 +----+-----------+----------+
