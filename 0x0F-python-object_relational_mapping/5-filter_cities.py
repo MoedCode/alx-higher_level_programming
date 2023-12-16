d64b505e3758bfcf5d1a4ab3156042752a1364f7
MoedCode <mohamed71291@gmail.com>
2023-12-16 03:22:40 +0200

    0x0F. PY ORM T 5 (automated command)

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    qur_exe = """SELECT cities.name FROM cities INNER JOIN states ON
+           states.id=cities.state_id WHERE states.name=%s"""
+    cursor.execute(qur_exe, (argv[4],))
+    rows = cursor.fetchall()
+    print(", ".join([row[0] for row in rows]))
