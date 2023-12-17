c74e9514202779b86d243932ff5b8f86df41dbb6
MoedCode <mohamed71291@gmail.com>
2023-12-17 18:18:59 +0200

    0x0F. PY ORM T 14 (automated command)

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """contains the class definition of a State and an instance """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class State(Base):
-    """states class """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+class State (Base):
+    """state class proto = Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
