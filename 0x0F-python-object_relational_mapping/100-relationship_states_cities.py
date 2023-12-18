b41b4ae236ff5579608543fcaa06ded7fd159d42
MoedCode <mohamed71291@gmail.com>
2023-12-18 04:48:38 +0200

    t100 (automated command)

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+from relationship_city import Base, City
+from relationship_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name='California')
+    city = City(name='San Francisco')
+    state.cities.append(city)
+    session.add(state)
+    session.add(city)
+    session.commit()
