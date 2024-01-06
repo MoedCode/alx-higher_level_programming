3372dac8a74e62f664962e3caec5d3aed519a8f2
MoedCode <mohamed71291@gmail.com>
2024-01-06 02:46:11 +0200

    0. cURL body size (automated command)

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # This script takes in a URL, sends a request, and displays the size of the body of the response
 
 # Sending a GET request to the specified URL, using curl in silent mode (-s) and writing the output to a temporary file (-o)
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -s "$1" | wc -c
