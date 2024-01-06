c39d257fb8a4a2d6411fc1e784d2108a60507ded
MoedCode <mohamed71291@gmail.com>
2024-01-06 03:44:48 +0200

    4. cURL headers (automated command)

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request with a specific header, and displays the response body
 
-curl -sH "X-School-User-Id: 98" "$1"
+curl -sH "X-School-User-Id: 98" "${1}"
