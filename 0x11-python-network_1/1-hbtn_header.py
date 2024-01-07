bc340ab4cb7f04ffe90f60a1fc32402051e7975a
MoedCode <mohamed71291@gmail.com>
2024-01-07 22:11:16 +0200

    1. Response header value #0 (automated command)

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,8 +8,11 @@ if __name__ == "___main__":
         print("Usage: ./1-hbtn_header.py <URL>")
         sys.exit(1)
     cmdURL = sys.argv[1]
+    print(f"cmd url {sys.argv[1]}")
     reqObj = urllib.request.Request(cmdURL)
+
     with urllib.request.urlopen(reqObj) as response:
         resObj = (response.headers).__dict__
+        print(f"response object {reqObj}")
         X_request_id = resObj.get("X_Request-Id")
         print(X_request_id)
