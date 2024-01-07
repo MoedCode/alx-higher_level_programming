18aa7907b5580d20238e92b65edc9780e8c13d59
MoedCode <mohamed71291@gmail.com>
2024-01-07 22:36:05 +0200

    1. Response header value #0 (automated command)

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 
-""" takes in a URL, sends a request to the URL and
-displays the value of the X-Request-Id variable found in the header of the response.
-"""
+""" takes in a URL, sends a request to the URL and displays the value."""
 
 import sys
 import urllib.robotparser
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./1-hbtn_header.py <URL>")
-        sys.exit(1)
+
     cmdURL = sys.argv[1]
     reqObj = urllib.request.Request(cmdURL)
 
