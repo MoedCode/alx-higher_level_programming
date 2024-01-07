3b479f2e85d6a5b09c6f829593eb54902cc75905
MoedCode <mohamed71291@gmail.com>
2024-01-07 04:40:04 +0200

    0. What's my status? #0 (automated command)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,10 @@ import urllib.request
 Url0 = "https://alx-intranet.hbtn.io/status"
 req = urllib.request.Request(Url0)
 
-with urllib.request.urlopen(req) as res:
+with urllib.request.urlopen(req) as response:
     # Displaying information about the response body
+    res = response.read()
     print("Body response:")
-    print("    - type:", type(res.read()))
-    print("    - content:", res.read())
-    print("    - utf8 content:", res.read().decode('utf-8'))
+    print("    - type:", type(res))
+    print("    - content:", res)
+    print("    - utf8 content:", res.decode('utf-8'))
