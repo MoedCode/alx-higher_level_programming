bad66a9bcf53ba8cf2dd66cec1009e3fe4b32b47
MoedCode <mohamed71291@gmail.com>
2024-01-07 23:57:32 +0200

    2. POST an email #0 (automated command)

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+""" takes in a URL, sends a request to the URL and displays the value."""
+
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    URL = sys.argv[1]
+    values = {"email": URL}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    reqObj = urllib.request.Request(URL, data)
+    with urllib.request.urlopen(reqObj) as response:
+        dec_res = response.read.decode('utf-8')
+        print(dec_res)
