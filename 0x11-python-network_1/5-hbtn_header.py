a558f6c91b6f3382692e53fd566079c1c62a8408
MoedCode <mohamed71291@gmail.com>
2024-01-08 03:01:57 +0200

    5. Response header value #1 (automated command)

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+""" takes in a URL, sends a request to the URL and displays the value."""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+
+    cmdURL = sys.argv[1]
+    response = requests.get(cmdURL)
+    print(response.headers["X-Request-Id"])
