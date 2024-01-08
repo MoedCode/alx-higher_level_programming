b3e2bc5ae3462ab90e728900eb33576bcc5eaae8
MoedCode <mohamed71291@gmail.com>
2024-01-08 03:22:11 +0200

    6. POST an email #1 (automated command)

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+""" takes in a URL, sends a request to the URL and displays the value."""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    URL = sys.argv[1]
+    email = sys.argv[2]
+    values = {"email": email}
+    response = requests.post(URL, data=values)
+    print(requests.text)
