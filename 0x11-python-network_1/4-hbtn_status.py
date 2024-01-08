c2e3df8cc994434cdc4cc009a1f7e2a6df2bdee8
MoedCode <mohamed71291@gmail.com>
2024-01-08 02:07:48 +0200

    4. What's my status? #1 (automated command)

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+"""fetches https://alx-intranet.hbtn.io/status using requests """
+
+import requests
+
+if __name__ == "__main__":
+    request = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t - type: {}".format(type(request)))
+    print("\t - content: {}".format(request.text))
