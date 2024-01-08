f02db311535f6467d4589378c9f759cb3ed1cc36
MoedCode <mohamed71291@gmail.com>
2024-01-08 06:02:00 +0200

    7. Error code #1 (automated command)

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+""",sends a request to the URL and displays the body of the response."""
+
 import sys
 import requests
 
-""",sends a request to the URL and displays the body of the response."""
 
 if __name__ == "__main__":
     url = sys.argv[1]
