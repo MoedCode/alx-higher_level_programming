112c1277e3da05f80ebe0724fc7f9a9ff8b93c45
MoedCode <mohamed71291@gmail.com>
2024-01-08 04:43:38 +0200

    9. My GitHub! (automated command)

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    URL = "https://api.github.com/user"
+
+    response = requests.get(URL, auth=(username, password))
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data["id"])
+    else:
+        print('None')
