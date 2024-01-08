14ff761b6b1f71961f47b5206a2c352d6afb6628
MoedCode <mohamed71291@gmail.com>
2024-01-08 04:27:00 +0200

    8. Search API (automated command)

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""fetches X-Request-Id in the response header"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    URL = "http://0.0.0.0:5000/search_user"
+    params = {"q": q}
+    response = requests.post(URL, data=params)
+    try:
+        json = response.json()
+        if not json:
+            print("No result")
+        else:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
