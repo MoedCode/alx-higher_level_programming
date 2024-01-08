9be3d0722e91949b1c12d2c12b8f470d7a08d0d2
MoedCode <mohamed71291@gmail.com>
2024-01-08 04:16:16 +0200

    7. Error code #1 (automated command)

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import requests
+import sys
+"""takes in a URL, sends a request to the URL and displays the body of the response."""
+if __name__ == "__main__":
+    URL = sys.argv[1]
+    response = requests.get(URL)
+
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
