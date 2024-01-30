0288c041ab3199842faf202a111704b302534e31
MoedCode <mohamed71291@gmail.com>
2024-01-30 21:42:58 +0200

    1. Write me (automated command)

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+
+const filePath = process.argv[2];
+const str = process.argv[3];
+
+
+if (!filePath && !str) {
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
+}
+
+
+fs.writeFile(filePath, str,(err)=>{
+    err && console.log(err);
+})
