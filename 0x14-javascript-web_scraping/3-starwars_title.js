ab6cc43e42a379ad6cfb3a8695fe766f9e266cdd
MoedCode <mohamed71291@gmail.com>
2024-01-31 01:49:18 +0200

    3. Star wars movie title (automated command)

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,12 @@ const { writeFile } = require('fs');
 
 const reqUrl = process.argv[2];
 const filePath = process.argv[3];
+
+if (!filePath && !reqUrl) {
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
+}
+
 request(reqUrl, (err, res, body) => {
   err && console.log(err.body);
   err && process.exit(1);
