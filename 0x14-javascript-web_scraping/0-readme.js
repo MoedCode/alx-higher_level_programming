fc8b2ea40de9361854d29a6b7128019140847fdf
MoedCode <mohamed71291@gmail.com>
2024-01-30 21:22:09 +0200

    0. Readme (automated command)

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,24 @@
 #!/usr/bin/node
-const { readFile } = require('node:fs');
 
+const fs = require('fs');
+
+// Get the file path from command-line arguments
 const filePath = process.argv[2];
-readFile(filePath, 'utf-8', (err, res) => {
-  err && console.error(err);
-  res && console.log(res);
+
+// Check if the file path is provided
+if (!filePath) {
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
+}
+
+// Read the content of the file in utf-8
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    // Print the error object if an error occurred during reading
+    console.error(err);
+    process.exit(1);
+  }
+
+  // Print the content of the file
+  console.log(data);
 });
