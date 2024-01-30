f4f31b85df387a10a36ed94e956ad05b4180d56d
MoedCode <mohamed71291@gmail.com>
2024-01-30 23:39:40 +0200

    2. Status code (automated command)

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const REQ  = require('request');
+
+req_url = process.argv[2];
+
+REQ (req_url, (err,res, body)=>{
+    err && console.log(err);
+    err && process.exit(1);
+    console.log(`code: ${res.statusCode}`);
+});
