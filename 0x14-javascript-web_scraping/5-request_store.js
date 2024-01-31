baea10919ab697a19cde4da6d1acd084bfe0eef5
MoedCode <mohamed71291@gmail.com>
2024-01-31 03:15:11 +0200

    5. Loripsum (automated command)

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const apiUrl = process.argv[2];
+const filePath = process.argv[3];
+
+if (!filePath && !apiUrl) {
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
+}
+request(apiUrl, (err, res, body) => {
+  if (err) {
+    console.err('Error', err);
+    return;
+  }
+  fs.writeFile(filePath, body, (err) => {
+    err && console.log(err);
+  });
+});
