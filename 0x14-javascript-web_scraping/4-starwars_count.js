1437f452344532d3608b6f23fa2060f9a2f165e8
MoedCode <mohamed71291@gmail.com>
2024-01-31 02:56:42 +0200

    4. Star wars Wedge Antilles (automated command)

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,10 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 
-
+if (!apiUrl) {
+  console.error('Usage: ./4-starwars_count.js <API_URL>');
+  process.exit(1);
+}
 
 request(apiUrl, (err, res, body) => {
   if (err) {
@@ -12,15 +15,21 @@ request(apiUrl, (err, res, body) => {
     process.exit(1);
   }
 
-  const filmsObj = JSON.parse(body).results;
+  try {
+    const filmsObj = JSON.parse(body).results;
 
-  if (!Array.isArray(filmsObj)) {
-    console.error('Unexpected response format from the API');
+    // Check if filmsObj is an array
+    if (Array.isArray(filmsObj)) {
+      const machArr = filmsObj.filter(film =>
+        film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+      );
+      console.log(machArr.length);
+    } else {
+      console.error('Unexpected response format from the API');
+      process.exit(1);
+    }
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
     process.exit(1);
   }
-
-  const machArr = filmsObj.filter(film =>
-    film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-  );
-  console.log(machArr.length);
 });
