5e8e57fbcad86a2cbd5c681bd9ec880a2bb1d87e
MoedCode <mohamed71291@gmail.com>
2024-01-31 03:52:27 +0200

    5. Loripsum (automated command)

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('<=>\n',error);
+    return;
+  }
+
+  const tasks = JSON.parse(body);
+
+  // Your logic to count completed tasks for each user goes here
+
+  // Output the result as specified
+  console.log(tasks);
+});
