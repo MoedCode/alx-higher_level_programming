794a5812991bf928a769fab4c139a4a70d4d00b5
MoedCode <mohamed71291@gmail.com>
2024-01-31 04:38:49 +0200

    8. Right order (automated command)

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,43 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Usage: ./101-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, function (charError, charResponse, charBody) {
+      if (charError) {
+        console.error(charError);
+        process.exit(1);
+      }
+
+      if (charResponse.statusCode !== 200) {
+        console.error(`Error: ${charResponse.statusCode}`);
+        process.exit(1);
+      }
+
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+    });
+  });
+});
