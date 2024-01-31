65b4e70e6b45091edabc99365a572410f8f2cf69
MoedCode <mohamed71291@gmail.com>
2024-01-31 04:42:54 +0200

    8. Right order (automated command)

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,41 +1,20 @@
 #!/usr/bin/node
-const request = require('request-promise-native'); // Use 'request-promise-native' for Promise-based requests
-
-async function getMovieData (movieId) {
-  const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-  try {
-    const response = await request(url);
-    return JSON.parse(response);
-  } catch (error) {
-    console.error(`Error fetching movie data: ${error.message}`);
-    process.exit(1);
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-}
+});
 
-async function printCharacters (characterUrls) {
-  for (const characterUrl of characterUrls) {
-    try {
-      const response = await request(characterUrl);
-      const characterData = JSON.parse(response);
-      console.log(characterData.name);
-    } catch (error) {
-      console.error(`Error fetching character data: ${error.message}`);
-      process.exit(1);
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
     }
-  }
+  });
 }
-
-async function main () {
-  const movieId = process.argv[2];
-
-  if (!movieId) {
-    console.error('Usage: ./101-starwars_characters.js <Movie ID>');
-    process.exit(1);
-  }
-
-  const movieData = await getMovieData(movieId);
-  const characters = movieData.characters;
-  await printCharacters(characters);
-}
-
-main();
